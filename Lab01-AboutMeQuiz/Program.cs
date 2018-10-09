1b4b4872c8ea5e8af1f643d795af4102bd810939
Danul De Leon <danul.deleon@gmail.com>
2018-10-08 19:40:37 -0700

    First question added

--- Lab01-AboutMeQuiz/Program.cs
+++ Lab01-AboutMeQuiz/Program.cs
@@ -4,9 +4,33 @@ namespace Lab01_AboutMeQuiz
 {
     class Program
     {
+        static class Count
+        {
+            public static int score;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's Take A Quiz About Danul!");
+            GuessAge();
+
+        }
+
+        static void GuessAge()
+        {
+            Console.Write("Can you guess Danul's age? ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == 30)
+            {
+                Console.WriteLine("Correct!  Danul is 30 years old.");
+                Count.score++;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect, Danul is actually 30 years old.");
+            }
+            Console.WriteLine($"Current Score: {Count.score}");
         }
     }
 }
